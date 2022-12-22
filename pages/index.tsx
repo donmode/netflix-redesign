c56d362ed7dbd6ca57d047d7a17f22c5656960ca
donmode <osanyinlokunoladiipo@gmail.com>
2022-12-22 04:49:21 +0100

    restrict scroll while modal is on

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,11 @@ const Home = ({
   const showModal = useRecoilValue(modalState)
 
   return !loading ? (
-    <div className="relative  h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div
+      className={`relative  h-screen bg-gradient-to-b lg:h-[140vh] ${
+        showModal && 'h-screen overflow-hidden'
+      }`}
+    >
       <Head>
         <title>HOME | NETFLIX</title>
         <link rel="icon" href="/favicon.ico" />
